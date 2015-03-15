6cb5c82fe9d6dc5d49e33801ef861f4b1427ee30
Luciano J Chaves <ljerezchaves@gmail.com>
2015-03-14 21:14:43 -0300

    Criando uma tag para monitorar os pacotes pela rede openflow.

---
+++ scratch/epc-of/epc-qos-tag.cc
@@ -0,0 +1,84 @@
+/* -*-  Mode: C++; c-file-style: "gnu"; indent-tabs-mode:nil; -*- */
+/*
+ * Copyright (c) 2015 University of Campinas (Unicamp)
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation;
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ * Author: Luciano Chaves <luciano@lrc.ic.unicamp.br>
+ */
+
+#include "epc-qos-tag.h"
+
+namespace ns3 {
+
+NS_LOG_COMPONENT_DEFINE ("EpcQosTag");
+NS_OBJECT_ENSURE_REGISTERED (EpcQosTag);
+
+EpcQosTag::EpcQosTag ()
+  : m_ts (Simulator::Now ().GetTimeStep ())
+{
+}
+
+EpcQosTag::EpcQosTag (uint32_t seq, uint32_t teid)
+  : m_ts (Simulator::Now ().GetTimeStep ()),
+    m_seq (seq),
+    m_teid (teid)
+{
+}
+
+TypeId 
+EpcQosTag::GetTypeId (void)
+{
+  static TypeId tid = TypeId ("ns3::EpcQosTag")
+    .SetParent<Tag> ()
+    .AddConstructor<EpcQosTag> ()
+  ;
+  return tid;
+}
+
+TypeId 
+EpcQosTag::GetInstanceTypeId (void) const
+{
+  return GetTypeId ();
+}
+
+uint32_t 
+EpcQosTag::GetSerializedSize (void) const
+{
+  return 16;
+}
+
+void 
+EpcQosTag::Serialize (TagBuffer i) const
+{
+  i.WriteU64 (m_ts);
+  i.WriteU32 (m_seq);
+  i.WriteU32 (m_teid);
+}
+
+void 
+EpcQosTag::Deserialize (TagBuffer i)
+{
+  m_ts = i.ReadU64 ();
+  m_seq = i.ReadU32 ();
+  m_teid = i.ReadU32 ();
+}
+
+void 
+EpcQosTag::Print (std::ostream &os) const
+{
+  os << " EpcQosTag teid=" << m_teid << " seq=" << m_seq << " ts=" << m_ts;
+}
+
+};  // namespace ns3
---
+++ scratch/epc-of/epc-qos-tag.h
@@ -0,0 +1,58 @@
+/* -*-  Mode: C++; c-file-style: "gnu"; indent-tabs-mode:nil; -*- */
+/*
+ * Copyright (c) 2015 University of Campinas (Unicamp)
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation;
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ * Author: Luciano Chaves <luciano@lrc.ic.unicamp.br>
+ */
+
+#ifndef EPC_QOS_TAG_H
+#define EPC_QOS_TAG_H
+
+#include "ns3/core-module.h"
+#include "ns3/network-module.h"
+   
+namespace ns3 {
+
+class Tag;
+
+/** 
+ * Tag used to measure traffic QoS over the OpenFlow LTE EPC network. 
+ */
+class EpcQosTag : public Tag
+{
+public:
+  static TypeId GetTypeId (void);
+  virtual TypeId GetInstanceTypeId (void) const;
+
+  /** Constructors */
+  EpcQosTag ();
+  EpcQosTag (uint32_t seq, uint32_t teid);
+
+  // Inherited from Tag
+  virtual void Serialize (TagBuffer i) const;
+  virtual void Deserialize (TagBuffer i);
+  virtual uint32_t GetSerializedSize () const;
+  virtual void Print (std::ostream &os) const;
+
+private:
+  uint64_t m_ts;    //!< Creating timestamp
+  uint32_t m_seq;   //!< Packet sequence number
+  uint32_t m_teid;  //!< GTP teid
+};
+
+};  // namespace ns3
+#endif // EPC_QOS_TAG_H
+
