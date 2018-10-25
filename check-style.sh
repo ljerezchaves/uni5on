993d348014f4d982a6bf501ee025db697c6d52d8
Luciano J Chaves <ljerezchaves@gmail.com>
2018-10-25 11:16:54 -0300

    Melhorando script de validar estilo de código.

--- check-style.sh
+++ check-style.sh
@@ -1,7 +1,26 @@
 #!/bin/bash
 
-ACTION=$1
-FILELIST=$(find scratch/svelte/ -name '*.cc' -o -name '*.h')
+function PrintHelp () {
+  echo "Usage: $0 <path_to_check> [--in-place]"
+  echo
+  exit 1
+}
+
+# Check for valid arguments
+if [ $# -lt 1 ] || [ $# -gt 2 ];
+then
+  echo "Invalid number of arguments"
+  PrintHelp
+fi;
+if [ $# -eq 2 ] && [ "$2" != "--in-place" ];
+then
+  echo "Invalid optional argument"
+  PrintHelp
+fi;
+
+BASEDIR=$1
+ACTION=$2
+FILELIST=$(find ${BASEDIR} -name '*.cc' -o -name '*.h')
 for I in ${FILELIST}
 do
     echo $I
@@ -9,7 +28,7 @@ do
       --in-place)
         ./utils/check-style.py -l3 --in-place --check-file=$I
       ;;
-    
+
       *)
         ./utils/check-style.py -l3 --diff --check-file=$I
       ;;
