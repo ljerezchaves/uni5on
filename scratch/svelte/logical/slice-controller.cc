49f91a43f7658b7f040e698417e32613ff50eea5
Luciano J Chaves <ljerezchaves@gmail.com>
2019-01-02 17:35:04 -0200

    Padrão desligado no P-GW adaptativo, link slicing e filas de prioridade.

--- scratch/svelte/logical/slice-controller.cc
+++ scratch/svelte/logical/slice-controller.cc
@@ -107,7 +107,7 @@ SliceController::GetTypeId (void)
     .AddAttribute ("PgwTftAdaptiveMode",
                    "P-GW TFT adaptive mechanism operation mode.",
                    TypeId::ATTR_GET | TypeId::ATTR_CONSTRUCT,
-                   EnumValue (OpMode::ON),
+                   EnumValue (OpMode::OFF),
                    MakeEnumAccessor (&SliceController::m_tftAdaptive),
                    MakeEnumChecker (OpMode::OFF,  "off",
                                     OpMode::ON,   "on",
