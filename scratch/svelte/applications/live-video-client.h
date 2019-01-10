4ca2372cde9295b8413a64a31973e29bcd7f133b
Luciano J Chaves <ljerezchaves@gmail.com>
2019-01-10 18:03:45 -0200

    Corrigindo cabeçalho licença GNU nos arquivos aplicação LiveVideo.

--- scratch/svelte/applications/live-video-client.h
+++ scratch/svelte/applications/live-video-client.h
@@ -1,6 +1,6 @@
 /* -*-  Mode: C++; c-file-style: "gnu"; indent-tabs-mode:nil; -*- */
 /*
- * Copyright (c) 2015 University of Campinas (Unicamp)
+ * Copyright (c) 2018 University of Campinas (Unicamp)
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 as
@@ -12,8 +12,7 @@
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ * along with this program; if not, see <http://www.gnu.org/licenses/>.
  *
  * Author: Luciano Chaves <luciano@lrc.ic.unicamp.br>
  */
