3f52fbe13c3f5571b9b8e86c57ea066f1d205722
Luciano J Chaves <ljerezchaves@gmail.com>
2019-01-11 20:47:35 -0200

    Cabeçalho de licença atualizado.

--- scratch/svelte/applications/live-video-server.h
+++ scratch/svelte/applications/live-video-server.h
@@ -1,6 +1,6 @@
 /* -*-  Mode: C++; c-file-style: "gnu"; indent-tabs-mode:nil; -*- */
 /*
- * Copyright (c) 2018 University of Campinas (Unicamp)
+ * Copyright (c) 2019 University of Campinas (Unicamp)
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 as
