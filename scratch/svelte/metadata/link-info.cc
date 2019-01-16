d5460873d40c553f5b32de0c8dbe3b9f0b502136
Luciano J Chaves <ljerezchaves@gmail.com>
2019-01-16 16:00:18 -0200

    Estilo de código.

--- scratch/svelte/metadata/link-info.cc
+++ scratch/svelte/metadata/link-info.cc
@@ -164,15 +164,7 @@ LinkInfo::GetLinkDir (uint64_t src, uint64_t dst) const
   NS_ASSERT_MSG ((src == GetSwDpId (0) && dst == GetSwDpId (1))
                  || (src == GetSwDpId (1) && dst == GetSwDpId (0)),
                  "Invalid datapath IDs for this connection.");
-
-  if (src == GetSwDpId (0))
-    {
-      return LinkInfo::FWD;
-    }
-  else
-    {
-      return LinkInfo::BWD;
-    }
+  return (src == GetSwDpId (0)) ? LinkInfo::FWD : return LinkInfo::BWD;
 }
 
 DpIdPair_t
